5c04a159875e80b5d065c4721a983d8dcdbee1e9
kevinnail <fumalicious@gmail.com>
2023-01-17 16:19:34 -0800

    skeleton files created: footer/ header/ main with basic css

---
+++ src/components/layout/Header/Header.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return <div className="header">This is an exquisite header</div>;
+}
---
+++ src/components/layout/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Main.css';
+export default function Main() {
+  return (
+    <div className="main">
+      How amazing can this content get?
+      <ul id="flag-list"></ul>
+    </div>
+  );
+}
