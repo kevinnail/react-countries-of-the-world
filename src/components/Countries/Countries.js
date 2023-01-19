b246c44bfbaead6da85a9fcba2d178b3576a09b0
kevinnail <fumalicious@gmail.com>
2023-01-18 16:18:52 -0800

    displaying countries with names

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Countries.css';
 
-export default function Countries({ name, setName }) {
+export default function Countries({ name }) {
   return (
     <div className="countries">
-      <h3>Bonaire, Sint Eustatius and Saba</h3>
+      <h3>{name}</h3>
       <img
         src="https://flagcdn.com/72x54/bq.png"
         width="72"
