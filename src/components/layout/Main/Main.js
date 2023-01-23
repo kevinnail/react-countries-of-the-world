f1103df075d56b9adf468693e547d8a21ec66018
kevinnail <fumalicious@gmail.com>
2023-01-23 08:07:53 -0800

    dropdown continent selection working

--- src/components/layout/Main/Main.js
+++ src/components/layout/Main/Main.js
@@ -1,4 +1,4 @@
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 import useCountries from '../../../hooks/useCountries.js';
 import Countries from '../../Countries/Countries.js';
@@ -6,11 +6,29 @@ import Countries from '../../Countries/Countries.js';
 export default function Main() {
   const { countries, error } = useCountries();
 
+  const [continent, setContinent] = useState('all');
+
+  const continents = [...new Set(countries.map(({ continent }) => continent))];
+  const filtered = countries.filter((country) => {
+    return country.continent === continent || continent === 'all';
+  });
+
   return (
     <div>
+      <select onChange={(e) => setContinent(e.target.value)}>
+        <option value="all">all</option>
+        {continents.map(
+          (continent) =>
+            continent && (
+              <option key={continent} value={continent}>
+                {continent}
+              </option>
+            )
+        )}
+      </select>
       <p className="error">{error}</p>
       <section className="main">
-        {countries.map((country) => (
+        {filtered.map((country) => (
           <Countries key={country.id} {...country} />
         ))}
       </section>
